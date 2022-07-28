e86b695e22e816a7629b1642c79ca2dab16a9a72
RoaaBaniOdeh <102085800+RoaaBaniOdeh@users.noreply.github.com>
2022-07-28 03:57:42 -0700

    Add files via upload

--- dist/main.js
+++ dist/main.js
@@ -1,12 +1,9 @@
-
-
-
 const fetchRecip = function(){
     $("#content").empty()
     let input=$("#ingred-name").val()
    $.get(`/recipes/${input}`,function(recipes){
     recipes.forEach(meal => {
-            $("#content").append(`<div class="meal-info"><div href=${meal.href}>${meal.title} </div>  
+            $("#content").append(`<div class="meal-info"><a href=${meal.href}>${meal.title} </a>  
             <div><ul>${meal.ingredients}<ul</div> 
             <img  src=${meal.thumbnail}>
             
