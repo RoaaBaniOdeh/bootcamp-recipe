349b0a3c9a150862688f7ef0d38aa99fd7f83f47
RoaaBaniOdeh <rooa.mohye@gmail.com>
2022-07-28 03:32:10 -0700

    v

--- dist/main.js
+++ dist/main.js
@@ -1,18 +1,10 @@
 
 
 
-
-
-
-
-
-
-
-
 const fetchRecip = function(){
     $("#content").empty()
     let input=$("#ingred-name").val()
-   $.get(`/recipes/${input}`,function(){
+   $.get(`/recipes/${input}`,function(recipes){
     recipes.forEach(meal => {
             $("#content").append(`<div class="meal-info"><div href=${meal.href}>${meal.title} </div>  
             <div><ul>${meal.ingredients}<ul</div> 
--- server.js
+++ server.js
@@ -7,50 +7,51 @@ app.use(express.static(path.join(__dirname,'dist')))
 app.use(express.static(path.join(__dirname,'node_modules')))
   
 let recipes = []
-
-const getRecpe = function (recipename, responses) {
- axioss.get('https://recipes-goodness.herokuapp.com/recipes/${recipename}')
- .then(function (response) {
-    // handle success
-
-
-   
-        recipes = result.data.results.map((recipe) => {
-          return {
-            ingredients: recipe.ingredients,
-            title: recipe.title,
-            thumbnail: recipe.thumbnail,
-            href: recipe.href,
-          };
-        });
-        responses.send(recipes);
-    })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
-
-
-
-}
-
-
 // Serving DATA
 app.get('/sanity',function(req,res){
    
     res.send("ok")
 })
--
-app.get('/recipes/:ingredient',function(req,res){  
-    res.send(res.data.ingredients)
-})
 
+
+
+
+
+
+
+
+
+    app.get('/recipes/:ingredient',function(req,response){  
+        axioss.get(`https://recipes-goodness.herokuapp.com/recipes/${req.params.ingredient}`)
+        .then((result) => {
+          recipes = result.data.results.map((recipe) => {
+            return {
+              ingredients: recipe.ingredients,
+              title: recipe.title,
+              thumbnail: recipe.thumbnail,
+              href: recipe.href,
+            };
+          });
+          console.log(recipes);
+          response.send(recipes)
+        });
+        
+      })
+    
 
 const port=8080
 app.listen(port,function(){
     console.log("Server is running on port"+port)
 
 })
+
+
+
+
+
+
+    
+    
+    
+      
+
